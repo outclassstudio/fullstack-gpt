119c27f8c852eaf432004c6830da63ca5b393b35
outclassstudio <mlimshs@gmail.com>
2024-12-03 16:28:57 +0900

    Feat: DocumentGPT(1)

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,27 +1,75 @@
 import streamlit as st
-import time
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.chat_models import ChatOpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.storage import LocalFileStore
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
 
 st.set_page_config(page_title="DocumentGPT", page_icon="📔")
-st.title("DocumentGPT")
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = []
+
+@st.cache_data(show_spinner="Embedding file...")
+def embed_file(file):
+    file_contnent = file.read()
+    file_path = f"./.cache/files/{file.name}"
+
+    with open(file_path, "wb") as f:
+        f.write(file_contnent)
+
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
 
 
 def send_message(message, role, save=True):
     with st.chat_message(role):
-        st.write(message)
+        st.markdown(message)
     if save:
         st.session_state["messages"].append({"message": message, "role": role})
 
 
-for msg in st.session_state["messages"]:
-    send_message(msg["message"], msg["role"], save=False)
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(message["message"], message["role"], save=False)
 
 
-message = st.chat_input("AI에게 질문을 남겨보아요")
+st.title("DocumentGPT")
+st.markdown(
+    """
+### Welcome!
+            
+Use this chatbot to ask question to an AI about your files!
 
-if message:
-    send_message(message, "human")
-    time.sleep(2)
-    send_message(f"You said: {message}", "ai")
+Upload your file on the sidebar.
+"""
+)
+
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt, pdf, docs file", type=["pdf", "txt", "docx"]
+    )
+
+if file:
+    retriever = embed_file(file)
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file..")
+
+    if message:
+        send_message(message, "human")
+        send_message("ㅋㅋㅋ", "ai")
+else:
+    st.session_state["messages"] = []
